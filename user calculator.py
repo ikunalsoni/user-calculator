4acfd4d7bb4efed4aab4fe55d2db0fdfa8b937a7
Kunal Soni <Ikunalsoni26@gmail.com>
2021-09-10 01:39:14 +0530

    Add files via upload

--- user calculator.py
+++ user calculator.py
@@ -1,6 +1,7 @@
-a = int(input('enter the first number'))
-b = int(input('Enter the second number'))
-while True:
+ch = 'y'
+while ch == 'y':
+    a = int(input('enter the first number'))
+    b = int(input('Enter the second number'))
     choice = input('Enter a operator(+,-,*,/,%):')
     if choice=='+':
         print('The answer is',a+b)
@@ -13,7 +14,14 @@ while True:
     elif choice=='%':
         print('The answer is',a%b)
     else:
-        print('Invalid operator') 
+        print('Invalid operator')
+        ch=input("Press y for continue")
+        
+    
+
+
+   
+
 
     
 
